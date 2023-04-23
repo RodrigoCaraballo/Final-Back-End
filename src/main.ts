7c00a8c28fa492ff6d7c018279ca7af345add804
Xtian <xtiancuenta1@gmail.com>
2023-04-23 09:44:21 -0300

    Se activó Cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { config } from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
