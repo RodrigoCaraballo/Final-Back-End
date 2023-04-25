0c5bb113c154581f48818101d8f9c80c2c4dac37
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-04-25 12:18:22 -0300

    Filter by email users

--- src/domain/repositories/student-evaluation.interface.repository.ts
+++ src/domain/repositories/student-evaluation.interface.repository.ts
@@ -1,9 +1,10 @@
 import { Observable } from "rxjs";
 import { StudentEvaluationModel } from "../model/student-evaluation.model";
 import { StudentEvaluationDTO } from "../dto";
+import { CriterionAverage } from "../../infrastructure/database/repositories/interfaces/interfaces.helpers";
 
 export interface IStudentEvaluationRepository {
     createEvaluation(data: StudentEvaluationDTO): Observable<StudentEvaluationModel>;
     getStudentEvaluation(studentId: string): Observable<StudentEvaluationModel>;
-    getTrainingEvaluations(trainingId: string): Observable<StudentEvaluationModel[]>
+    getTrainingEvaluations(trainingId: string): Observable<CriterionAverage[]>
 }
